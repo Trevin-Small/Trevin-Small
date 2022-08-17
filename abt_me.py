9f8d0a589152f8ca212035d7a78d884a2bc25d88
Trevin Small <trevincub03@gmail.com>
2022-08-17 01:06:54 -0400

    Updated code pic

---
+++ abt_me.py
@@ -0,0 +1,20 @@
+class Trevin():
+
+  def __init__(self):
+
+    self.interestedIn = """Almost everything STEM, software and hardware, including programming, robotics, MCUs,
+                        3D printing, and electronics. I also enjoy cooking and rock climbing."""
+
+    self.educationAt = """Computer Science student at Purdue University. I plan to take part in either the
+                       Machine Learning focus track, or the Software Engineering track."""
+
+    self.workingOn = """A mix of everything, my work is all over the place. I am however especially interested
+                     in web  development and lower level code for MCU's and other hardware."""
+
+    self.knowledgeableIn = [
+                            "JS", "NodeJS", "C", "Python", "Java", "Solidity",
+                            "HTML", "CSS", "Webpack", "Firebase", "Google Cloud"
+                            ]
+
+  def likesCoffee():
+    return True
